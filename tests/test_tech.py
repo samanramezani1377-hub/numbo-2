7014f0f0a3d163be35263ee50c1f07cea6a132c0
samanramezani1377-hub <saman.ramezani1377@gmail.com>
2026-09-26 02:50:41 +0330

    Test stable technology result formatting

--- tests/test_tech.py
+++ tests/test_tech.py
@@ -1,14 +1,27 @@
-from numbo.utils.tech import detect_technologies
+from numbo.utils.tech import detect_technologies, format_technologies
+
 
 def test_woocommerce_strong_signature():
     result = detect_technologies("<script src='/wp-content/plugins/woocommerce/assets/js/wc-add-to-cart.js'></script>")
     names = {x["name"] for x in result}
     assert "WooCommerce" in names
 
+
 def test_weak_word_not_enough():
     result = detect_technologies("<html><body>wordpress training course</body></html>")
     assert "WordPress" not in {x["name"] for x in result}
 
+
 def test_nextjs_signature():
     result = detect_technologies("<script src='/_next/static/chunks/app.js'></script>")
     assert "Next.js" in {x["name"] for x in result}
+
+
+def test_technology_format_is_clean_and_stable():
+    result = detect_technologies(
+        "<html><script src='/wp-content/plugins/woocommerce/assets/js/wc-add-to-cart.js'></script>"
+        "<script src='/_next/static/chunks/app.js'></script></html>"
+    )
+    formatted = format_technologies(result)
+    assert "(" not in formatted
+    assert formatted.split("; ") == ["WordPress", "WooCommerce", "Next.js"]
